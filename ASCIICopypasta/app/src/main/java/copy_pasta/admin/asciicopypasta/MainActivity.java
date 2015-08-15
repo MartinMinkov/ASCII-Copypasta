00363301c4391f008c72b447e8f8ceaf0f007da2
Scott-Plummer <Scott.Plummer18@gmail.com>
2015-08-14 20:58:36 -0700

    Removed orientation lock and added new icon art

--- ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MainActivity.java
+++ ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MainActivity.java
@@ -1,23 +1,10 @@
 package copy_pasta.admin.asciicopypasta;
 
 import android.app.FragmentManager;
-import android.app.ListFragment;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.ClipboardManager;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
