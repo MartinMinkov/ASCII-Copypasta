0972fdbcb0e6373db031ac7b0dbf2a07edbf6412
Scott-Plummer <Scott.Plummer18@gmail.com>
2015-08-18 21:38:11 -0700

    Added category fragment, and removed undeeded .idea folder

--- ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MainActivity.java
+++ ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MainActivity.java
@@ -2,42 +2,69 @@ package copy_pasta.admin.asciicopypasta;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+    private FragmentManager fragManager = getFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentManager fragManager = getFragmentManager();
         if(fragManager.findFragmentById(android.R.id.content) == null) {
             MemeListFragment memeFragment = new MemeListFragment();
-            fragManager.beginTransaction().add(android.R.id.content, memeFragment).commit();
+            fragManager.beginTransaction().
+                    add(android.R.id.content, memeFragment).commit();
+
         }
     }
-
+    @Override
+    public void onBackPressed() {
+        if(fragManager.getBackStackEntryCount() != 0) {
+            fragManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.categories:
+                categoryMenu();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
+    public void categoryMenu() {
+            SettingsFragment settingsFragment = new SettingsFragment();
+            fragManager.beginTransaction().addToBackStack(null).replace(android.R.id.content, settingsFragment).commit();
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
 
-        return super.onOptionsItemSelected(item);
+    }
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
--- ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MemeListFragment.java
+++ ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MemeListFragment.java
@@ -5,10 +5,14 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
