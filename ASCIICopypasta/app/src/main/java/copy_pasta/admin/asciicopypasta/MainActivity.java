20d829f08f86f1154e4976cfa524dee455f2fe05
Martin Minkov <minkovlmartin@gmail.com>
2015-09-03 04:43:15 -0700

    Fixed bug with app name and actionbar name

--- ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MainActivity.java
+++ ASCIICopypasta/app/src/main/java/copy_pasta/admin/asciicopypasta/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Main Menu");
     }
     /*Intent for memeListFragment hosted Activity*/
     public void switchToMemeList(View view) {
